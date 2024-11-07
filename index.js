3e3e31b313be93b924c666a72dd888800a96f564
Vir1dian <132039432+Vir1dian@users.noreply.github.com>
2024-11-06 17:20:12 -0800

    Styling changes, WIP: matrix builder interface

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 "use strict";
+function showBuildOption() {
+    const option_default = document.getElementById('build_matrix_default');
+    const option_matlabstring = document.getElementById('build_matrix_matlabstring');
+    const option_example = document.getElementById('build_matrix_example');
+    const selected = document.querySelector('#build_options');
+    switch (selected.value) {
+        case "matlabstring":
+            option_default.style.display = "none";
+            option_matlabstring.style.display = "";
+            option_example.style.display = "none";
+            break;
+        case "example":
+            option_default.style.display = "none";
+            option_matlabstring.style.display = "none";
+            option_example.style.display = "";
+            break;
+        default:
+            option_default.style.display = "";
+            option_matlabstring.style.display = "none";
+            option_example.style.display = "none";
+    }
+}
 const selected_matrix = matrices[5];
 function matrixOperation() {
-    const operation_input = document.querySelector('#interface_operations');
-    switch (operation_input.value) {
+    const selected = document.querySelector('#interface_operations');
+    switch (selected.value) {
         case "rref":
             // console.log("RREF");
             gaussJordan(selected_matrix);
