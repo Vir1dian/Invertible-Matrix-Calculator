be0dd9a685764576851a26026ab8296f7088010a
Vir1dian <132039432+Vir1dian@users.noreply.github.com>
2025-06-20 11:18:54 -0700

    replaced all inline function calls in html file with event listeners

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,8 @@ function loadAll() {
   MatrixBuilderFunctions.exampleMatrix.populateExampleList(matricesForRREF, matricesToInvert);
 
   document.querySelector<HTMLSelectElement>('#build_options')?.addEventListener("change", showBuildOption);
+  document.querySelector<HTMLButtonElement>('#submit_dimensions')?.addEventListener("click", MatrixBuilderFunctions.default.setMatrixInputs);
+  document.querySelector<HTMLButtonElement>('#submit_matrix_default')?.addEventListener("click", MatrixBuilderFunctions.default.createMatrix);
+  document.querySelector<HTMLButtonElement>('#submit_matrix_matlabstring')?.addEventListener("click", MatrixBuilderFunctions.matlabString.createMatrix);
+  document.querySelector<HTMLButtonElement>('#interface_submit')?.addEventListener("click", matrixOperation);
 }
